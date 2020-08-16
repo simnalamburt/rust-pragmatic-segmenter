c6f6c8e7451192da597fef4c8cfd6b271f4c6321
Hyeon Kim <simnalamburt@gmail.com>
2020-08-16 22:12:12 +0900

    test_all.rs: Do not early exit

--- tests/test_all.rs
+++ tests/test_all.rs
@@ -64,13 +64,28 @@ fn test_all() -> TestResult {
     let inputs = BufReader::new(XzDecoder::new(File::open("tests/fixtures/inputs.xz")?));
     let outputs = BufReader::new(XzDecoder::new(File::open("tests/fixtures/outputs.xz")?));
 
-    for (input, output) in std::iter::Iterator::zip(inputs.lines(), outputs.lines()) {
+    let mut bad = 0;
+    for each in std::iter::Iterator::zip(inputs.lines(), outputs.lines()).enumerate() {
+        let (i, (input, output)) = each;
         let input: String = serde_json::from_str(&input?)?;
 
         let expected: Vec<String> = serde_json::from_str(&output?)?;
         let actual: Vec<_> = segmenter.segment(&input).collect();
-        assert_eq!(actual, expected);
+
+        if actual != expected {
+            bad += 1
+        }
+        if i % 250 == 0 {
+            println!(
+                "{:5}: Good={:5}, Bad={:5}, {:5}%",
+                i,
+                i + 1 - bad,
+                bad,
+                (i + 1 - bad) as f64 / (i + 1) as f64 * 100.0
+            );
+        }
     }
+    assert_eq!(bad, 0);
 
     Ok(())
 }
