725ccedf21ac6aa56baeebdec16b6e1c1438730b
Hyeon Kim <simnalamburt@gmail.com>
2020-08-20 18:05:26 +0900

    Run test_huge.rs in parallel

--- tests/test_huge.rs
+++ tests/test_huge.rs
@@ -1,6 +1,10 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader};
+
+use indicatif::{ProgressBar, ProgressStyle};
+use rayon::prelude::*;
+use serde_json::from_str;
 use xz2::read::XzDecoder;
 
 use pragmatic_segmenter::Segmenter;
@@ -13,29 +17,45 @@ fn test_all() -> TestResult {
 
     let inputs = BufReader::new(XzDecoder::new(File::open("tests/fixtures/inputs.xz")?));
     let outputs = BufReader::new(XzDecoder::new(File::open("tests/fixtures/outputs.xz")?));
+    let dataset: Vec<_> = inputs
+        .lines()
+        .zip(outputs.lines())
+        .map(|(input, output)| {
+            let input: String = from_str(&input?)?;
+            let output: Vec<String> = from_str(&output?)?;
 
-    let mut bad = 0;
-    for each in std::iter::Iterator::zip(inputs.lines(), outputs.lines()).enumerate() {
-        let (i, (input, output)) = each;
-        let input: String = serde_json::from_str(&input?)?;
+            Ok((input, output))
+        })
+        .collect::<io::Result<_>>()?;
+    let count = dataset.len();
+    let bar = ProgressBar::new(count as u64);
+    bar.set_draw_delta(100);
+    bar.set_style(ProgressStyle::default_bar().template("
+{percent}% {wide_bar} {pos:>5}/{len}
+{elapsed} passed, Currrent speed: {per_sec}, {eta} left
+",
+    ));
 
-        let expected: Vec<String> = serde_json::from_str(&output?)?;
-        let actual: Vec<_> = segmenter.segment(&input).collect();
+    let good = dataset
+        .par_iter()
+        .map(|(input, expected)| {
+            bar.inc(1);
+            let actual: Vec<_> = segmenter.segment(&input).collect();
+            actual == *expected
+        })
+        .filter(|&b| b)
+        .count();
+    bar.finish();
 
-        if actual != expected {
-            bad += 1
-        }
-        if i % 250 == 0 {
-            println!(
-                "{:5}: Good={:5}, Bad={:5}, {:5}%",
-                i,
-                i + 1 - bad,
-                bad,
-                (i + 1 - bad) as f64 / (i + 1) as f64 * 100.0
-            );
-        }
-    }
-    assert_eq!(bad, 0);
+    assert_eq!(
+        count,
+        good,
+        "Total={}, Good={}, Bad={}, ({:.3}%)",
+        count,
+        good,
+        count - good,
+        good as f64 / count as f64 * 100.0
+    );
 
     Ok(())
 }
