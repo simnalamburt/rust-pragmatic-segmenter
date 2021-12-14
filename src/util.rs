ccccab2c22d446262b1741e348f2b97b435f7ff5
Hyeon Kim <simnalamburt@gmail.com>
2021-12-15 04:02:16 +0900

    util.rs: Run `cargo fmt`

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,9 @@
 use onig::{Error, Regex, RegexOptions, Syntax};
 
 pub fn re(regex: &str) -> Result<Regex, Error> {
-    Regex::with_options(
-        regex,
-        RegexOptions::REGEX_OPTION_NONE,
-        Syntax::ruby(),
-    )
+    Regex::with_options(regex, RegexOptions::REGEX_OPTION_NONE, Syntax::ruby())
 }
 
 pub fn re_i(regex: &str) -> Result<Regex, Error> {
-    Regex::with_options(
-        regex,
-        RegexOptions::REGEX_OPTION_IGNORECASE,
-        Syntax::ruby(),
-    )
+    Regex::with_options(regex, RegexOptions::REGEX_OPTION_IGNORECASE, Syntax::ruby())
 }
